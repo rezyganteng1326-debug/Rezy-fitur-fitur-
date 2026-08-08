8b97caf7a2192c77af1063079b3418e94fa8d1bd
rezyganteng1326-debug <rezyganteng1326@gmail.com>
2026-08-08 10:19:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ const CPU_COUNT = cpus().length
 
 Object.assign(globalThis, {
 
-   ownerName: '𝑣𝑖𝑡𝑎 - 𝑖𝑚𝑢𝑡', // ganti inii 
+   ownerName: 'Rezyganteng', // ganti inii 
    ownerNumber: '62xxx', // ganti ini nomor owner 
-   botName: '𝗒𝗎𝗄𝗂 𝖨𝗍𝗈𝗌𝖾', // ganti 
-   footer: '🍃 𝗒𝗎𝗄𝗂 𝖨𝗍𝗈𝗌𝖾', // ganti 
+   botName: 'RezyBotwawa', // ganti 
+   footer: '🍃 bot wawa', // ganti 
    botNumber: '6289525408623', // ganti ini nomor mu bot 
 
    pairingCode: true, // ga usah gnti 
@@ -17,7 +17,7 @@ Object.assign(globalThis, {
 
    stickerPackName: '𝗂𝗇𝗌𝗍𝖺𝗀𝗋𝖺𝗆', // bebas gnti atau tidak
 
-   stickerPackPublisher: '@𝖿𝗂𝗍𝗒𝖺_𝗍𝖺𝖺', // bebas gnti atau tidak 
+   stickerPackPublisher: '@Rezy', // bebas gnti atau tidak 
 
    googleApiKey: '', // apikey mu sendiri di gemini 
 
